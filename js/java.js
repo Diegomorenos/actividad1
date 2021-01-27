0123a87d3f4dc97ab9c3d776904659455037a201
unknown <d1360ms@gmail.com>
2021-01-27 10:09:45 -0500

    javascript

---
+++ js/java.js
@@ -0,0 +1,17 @@
+function validacioncampos() {
+ /*   var  tipo = document.getElementById("tipo").value;
+    var  nombre = document.getElementById("nombre").value;
+    
+    if (tipo==""){
+        alert("El Campo Tipo de Mensaje Esta Vacio");
+        document.getElementById("tipo").focus();
+    }
+
+    if (nombre==""){
+        alert("El Campo Nombre Esta Vacio");
+        document.getElementById("name").focus();
+    }*/
+    
+    
+}
+
