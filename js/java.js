850baed4cc1eaeb3a5c3642c411043b943050320
unknown <d1360ms@gmail.com>
2021-01-27 10:58:30 -0500

    continua javascript

--- js/java.js
+++ js/java.js
@@ -1,8 +1,11 @@
 function validacioncampos() {
- /*   var  tipo = document.getElementById("tipo").value;
+    var  tipo = document.getElementById("tipo").value;
     var  nombre = document.getElementById("nombre").value;
-    
-    if (tipo==""){
+    var  email = document.getElementById("email").value;
+    var  celular = document.getElementById("celular").value;
+    var mensaje = "";
+
+   /*if (tipo==""){
         alert("El Campo Tipo de Mensaje Esta Vacio");
         document.getElementById("tipo").focus();
     }
@@ -12,6 +15,21 @@ function validacioncampos() {
         document.getElementById("name").focus();
     }*/
     
-    
+    if (tipo =="" || nombre =="" || email =="" || celular ==""){
+        
+        if (tipo ==""){
+            mensaje +="El Campo Tipo de Mensaje Esta Vacio"
+        }
+        if (nombre =="") {
+            mensaje +="\nEl Campo Nombre Esta Vacio"
+        }
+        if (email =="") {
+            mensaje +="\nEl Campo E-mail Esta Vacio"
+        }
+        if (celular =="") {
+            mensaje +="\nEl Campo Celular Esta Vacio"
+        alert (mensaje)
+        }
+    }
 }
 
